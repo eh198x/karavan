cdb3d7c7ee82d39e17273f32846ee41c0ab6ad29
Elias H. <eh198x>
2024-05-07 14:33:12 +0300

    properly calling external groovy script

--- ftp.groovy
+++ ftp.groovy
@@ -1,9 +1,12 @@
-//evaluate(new File("./tools.groovy"))
+GroovyShell shell = new GroovyShell()
+def tools = shell.parse(new File('mytools.groovy'))
 
 //import groovy.xml.XmlParser
+//import mytools
+
 
 import java.util.logging.Logger
-import tools
+
 
 //import groovy.json.JsonSlurper
 
@@ -11,6 +14,7 @@ import tools
 Logger LOGGER = Logger.getLogger("")
 //LOGGER.info("=====FTP-Received a new file upload...\n")
 
+ 
 tools.process(exchange, LOGGER)
 
 /*
