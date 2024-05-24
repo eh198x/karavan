a916eb05939785a52fb5911437f62e3c88f69418
Elias H. <eh198x>
2024-05-24 16:54:49 +0300

    custom logging does not work

--- logger.wf.groovy
+++ logger.wf.groovy
@@ -2,16 +2,22 @@ import java.util.logging.Logger
 import org.apache.camel.Exchange 
 import java.io.File
 
+
 Logger LOGGER = Logger.getLogger("")
 //LOGGER.info("Received a new file upload...\n")
 process(exchange, LOGGER)
 
-def process(Exchange exchange, Logger LOGGER) throws Exception {
-   
+def process(Exchange exchange, Logger LOGGER) throws Exception {    
+
+    def exchangeId = exchange.getExchangeId() // Access the Exchange ID
+
     def myFile = exchange.getIn().getBody(File.class); //Works
     
-    LOGGER.info("Workflow started for file:" + myFile )
- 
-   
+      exchange.setProperty("myFile", myFile.absolutePath) // Use absolute path for clarity
+
+
+    LOGGER.info("EXCHANGE ID ${exchangeId} - Workflow started for file: ${myFile}")
+
+
 }
 
