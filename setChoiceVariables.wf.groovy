f1430d18b4bf84a6d82e9548321f22e01b99424b
Elias H. <eh198x>
2024-05-29 12:32:56 +0300

    code reorg

--- setChoiceVariables.wf.groovy
+++ setChoiceVariables.wf.groovy
@@ -7,25 +7,7 @@ process(exchange, LOGGER)
 
 def process(Exchange exchange, Logger LOGGER) throws Exception {
     try {
-        def exchangeId = exchange.getExchangeId()     
-
-        def myFile = exchange.getIn().getBody(File.class);
-
-        def filePath = myFile.absolutePath
-        //String exceptionMessage = exchange.getIn().getHeader("ExceptionMessage", String.class)
-        LOGGER.info("EXCHANGE ID: ${exchangeId} - Workflow started for file: " + filePath)
-
-        def normalizedPath = filePath.replaceAll("[/\\\\]+", "/") // Normalize path separators
-        def country = normalizedPath.tokenize("/")[-2] // Get the second-to-last token
-
-        println "Last folder: $country" // This will output the country name
-
-        //def recid = id.toInteger()
-        exchange.setProperty("Country", country)
-        //exchange.setProperty("update_workflow_status", "UPDATE public.workflow SET status='AA' WHERE id = $recid");
-        LOGGER.info("Setting Country: $country")
-
-
+        identifyCountryfromFolder(exchange,LOGGER)
     } catch (Exception e) {
         // Log the exception details
         LOGGER.info("EXCHANGE ID: ${exchangeId} - Exception occurred: ${e.message}")
@@ -34,4 +16,24 @@ def process(Exchange exchange, Logger LOGGER) throws Exception {
         // Rethrow the exception if needed
         throw e
     }
+}
+
+def identifyCountryfromFolder(Exchange exchange, Logger LOGGER) throws Exception {
+    def exchangeId = exchange.getExchangeId()     
+
+    def myFile = exchange.getIn().getBody(File.class);
+
+    def filePath = myFile.absolutePath
+    //String exceptionMessage = exchange.getIn().getHeader("ExceptionMessage", String.class)
+    LOGGER.info("EXCHANGE ID: ${exchangeId} - Workflow started for file: " + filePath)
+
+    def normalizedPath = filePath.replaceAll("[/\\\\]+", "/") // Normalize path separators
+    def country = normalizedPath.tokenize("/")[-2] // Get the second-to-last token
+
+    println "Last folder: $country" // This will output the country name
+
+    //def recid = id.toInteger()
+    exchange.setProperty("Country", country)
+    //exchange.setProperty("update_workflow_status", "UPDATE public.workflow SET status='AA' WHERE id = $recid");
+    LOGGER.info("Setting Country: $country")
 }
