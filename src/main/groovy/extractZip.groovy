94d8ebb7997a850f40f81efa8e63a1757e6eb55b
eh198x <eh198x@outlook.com>
2024-07-08 16:18:09 +0300

    extract zip with more than 1 files. Save them to a separate directory using a groovy script. Select the xml file and save it separately

---
+++ src/main/groovy/extractZip.groovy
@@ -0,0 +1,77 @@
+import java.util.zip.*
+import java.util.logging.Logger
+import org.apache.camel.Exchange
+  
+//String zipFileName = myFile.absolutePath
+//String inputDir = "Fileshare/1-INPUT"
+def outputDir = "Fileshare/2-EXTRACTS"
+
+Logger LOGGER = Logger.getLogger("")
+//LOGGER.info("Received a new file upload...\n")
+
+process(outputDir, exchange, LOGGER)
+
+//UnZip archive
+def process(String outputDir, Exchange exchange, Logger LOGGER) throws Exception {
+    def myFile = exchange.getIn().getBody(File. class)
+    def filePath = myFile.absolutePath
+    //Relative path 
+    //String pathWithoutFilename = myFile.getParentFile().getPath();
+    //Absolute Path
+    String pathWithoutFilename = myFile.getParentFile().getAbsolutePath();
+    String parentFilename = myFile.getParentFile().getName();
+    outputDir = outputDir + File.separator + parentFilename
+    LOGGER.info("Printing parent file name:" + parentFilename)
+    LOGGER.info("Printing outputDir:" + outputDir)
+    def exchangeId = exchange.getExchangeId()
+    byte [] buffer = new byte [1024]
+    ZipInputStream zis = new ZipInputStream(new FileInputStream(filePath))
+    ZipEntry zipEntry = zis.getNextEntry()
+    while (zipEntry != null) {
+        File newFile = new File(outputDir + File.separator + exchangeId + File.separator, zipEntry.name)
+        //File newFile = new File(outputDir+ File.separator + exchangeId + File.separator, pathWithoutFilename)
+        if (zipEntry.isDirectory()) {
+            if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                throw new IOException("Failed to create directory " + newFile)
+            }
+        } else {
+            // fix for Windows-created archives
+            File parent = newFile.parentFile
+            if (!parent.isDirectory() && !parent.mkdirs()) {
+                throw new IOException("Failed to create directory " + parent)
+            }
+            // write file content
+            FileOutputStream fos = new FileOutputStream(newFile)
+            int len = 0
+            while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len)
+            }
+            fos.close()
+        }
+        zipEntry = zis.getNextEntry()
+    }
+    zis.closeEntry()
+    zis.close()
+}
+
+//LOGGER.info("Printing file path without filename:" + pathWithoutFilename)
+//process(exchange, LOGGER)
+//extract(exchange, LOGGER)
+
+/*
+//Zip files  
+ZipOutputStream zipFile = new ZipOutputStream(new FileOutputStream(zipFileName))  
+new File(inputDir).eachFile() { file -> 
+  //check if file
+  if (file.isFile()){
+    zipFile.putNextEntry(new ZipEntry(file.name))
+    def buffer = new byte[file.size()]  
+    file.withInputStream { 
+      zipFile.write(buffer, 0, it.read(buffer))  
+    }  
+    zipFile.closeEntry()
+  }
+}  
+zipFile.close()  
+*/
+
